3cb9d2f99f7a5bf83daa1af8ef847b2a8191e95b
srijareddy-27 <srijareddy.yelapati@gmail.com>
2026-03-11 12:27:58 +0530

    Implement Medicine class with properties and methods
    Added properties and methods for Medicine class.

--- src/Pharmatrack/Medicine.java
+++ src/Pharmatrack/Medicine.java
@@ -2,9 +2,37 @@ package Pharmatrack;
 
 public class Medicine {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    private String name;
+    private String code;
+    private String batch;
+    private int quantity;
+    private String expiry;
 
-	}
+    // Constructor
+    public Medicine(String name, String code, String batch, int quantity, String expiry) {
+        this.name = name;
+        this.code = code;
+        this.batch = batch;
+        this.quantity = quantity;
+        this.expiry = expiry;
+    }
 
+    public String getCode() {
+        return code;
+    }
+
+    // displayMedicine()
+    public void displayMedicine() {
+        System.out.println("Name: " + name);
+        System.out.println("Code: " + code);
+        System.out.println("Batch: " + batch);
+        System.out.println("Quantity: " + quantity);
+        System.out.println("Expiry: " + expiry);
+        System.out.println("---------------------------");
+    }
+
+    // updateQuantity()
+    public void updateQuantity(int qty) {
+        quantity = qty;
+    }
 }
